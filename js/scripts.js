d571fa8b6b46db17ea40e647569d3e5dd6bbd695
ma-za-kpe <makpalyy@gmail.com>
2018-06-30 11:51:53 +0300

    add the third spec of the year being divisible by 100 in the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@
 //BUISNESS LOGIC
 
 var leapYear = function (year) {
-  if (year%4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year%4 === 0) {
     return true;
   } else {
     return false;
