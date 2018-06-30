c31a0072fafd859d470092ea6249dac510775875
ma-za-kpe <makpalyy@gmail.com>
2018-06-30 11:36:21 +0300

    add code to the user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+//BUISNESS LOGIC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI logi
+
+  $(document).ready(function () {
+
+    $("form#leap-year").submit(function (event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+
+  });
