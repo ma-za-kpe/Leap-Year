e40a58f8ba2c8f97e93edd63a6b0115d43b1c627
ma-za-kpe <makpalyy@gmail.com>
2018-06-30 11:57:35 +0300

    add the fourth spec of the year being divisible by 400 in the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,12 @@
 //BUISNESS LOGIC
 
 var leapYear = function (year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year%4 === 0) {
+ if ((year%4 === 0) && (year % 100 === 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
-  return false;
-}
+};
 
 
 
